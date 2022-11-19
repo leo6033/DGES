5a60fc5399507a840f2929c855d2c7d01deb40a4
leo6033 <long452a@163.com>
2022-11-20 01:23:04 +0800

    新增事件监听接口，添加回调delegate

---
+++ Assets/com.disc0ver.Event/Runtime/EventManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Disc0ver.Event
+{
+    /// <summary>
+    /// EventManager 基类
+    /// </summary>
+    public abstract class AEventManager
+    {
+        private readonly Dictionary<Type, List<IBaseEventListener>> _eventSubscriptDict = new Dictionary<Type, List<IBaseEventListener>>();
+
+        public void AddListener<TEvent>(IBaseEventListener eventListener) where TEvent: struct, IBaseEvent
+        {
+            if(!_eventSubscriptDict.TryGetValue(typeof(TEvent), out List<IBaseEventListener> subscriptList))
+            {
+                subscriptList = new List<IBaseEventListener>();
+            }
+
+            subscriptList.Add(eventListener);
+            _eventSubscriptDict[typeof(TEvent)] = subscriptList;
+        }
+
+        public void RemoveListener<TEvent>(IBaseEventListener eventListener) where TEvent: struct, IBaseEvent
+        {
+            if (!_eventSubscriptDict.TryGetValue(typeof(TEvent), out List<IBaseEventListener> subscriptList))
+            {
+                return;
+            }
+
+            subscriptList.Remove(eventListener);
+            if(subscriptList.Count == 0)
+            {
+                _eventSubscriptDict.Remove(typeof(TEvent));
+            }
+        }
+
+        public void OnBroadCastEvent<TEvent>(TEvent newEvent) where TEvent : struct, IBaseEvent
+        {
+            if(!_eventSubscriptDict.TryGetValue(typeof(TEvent), out List<IBaseEventListener> subscriptList))
+            {
+                return;
+            }
+
+            for(int i = subscriptList.Count - 1; i >= 0; i--)
+            {
+                IEventListener<TEvent> listener = subscriptList[i] as IEventListener<TEvent>;
+                listener?.OnReceiveEvent(newEvent);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 全局 EventManager,使用 EventManager.Instance 调用相关方法
+    /// </summary>
+    public class EventManager : AEventManager
+    {
+        private static EventManager _instance;
+
+        public static EventManager Instance => _instance ??= new EventManager();
+    }
+}
---
+++ Assets/com.disc0ver.Event/Runtime/IEventListener.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Disc0ver.Event
+{
+    /// <summary>
+    /// 事件监听基础接口
+    /// </summary>
+    public interface IBaseEventListener { }
+
+    /// <summary>
+    /// 单事件监听，一个 IEventListener 支持监听一个事件
+    /// 假如类 A 要监听 EventA, EventB
+    /// </summary>
+    /// <typeparam name="TEvent"> 需要监听的事件 </typeparam>
+    /// <example>
+    /// <code>  class A: IEventListener&lt;EventA&gt;  IEventListener&lt;EventB&gt; </code>
+    /// </example>
+    public interface IEventListener<in TEvent> : IBaseEventListener where TEvent: struct, IBaseEvent
+    {
+        /// <summary>
+        /// 接收到事件消息时的处理函数
+        /// </summary>
+        /// <param name="newEvent"></param>
+        public void OnReceiveEvent(TEvent newEvent);
+    }
+
+    /// <summary>
+    /// 多事件监听，在 ListenEvents 中加入需要监听的事件
+    /// 由类内部去做各种 Event 的处理
+    /// </summary>
+    public interface IEventsListener : IBaseEventListener
+    {
+        /// <summary>
+        /// 监听的事件
+        /// </summary>
+        public abstract List<IBaseEvent> ListenEvents { get; }
+
+        /// <summary>
+        /// 接收到事件消息时的处理函数
+        /// </summary>
+        /// <param name="newEvent"></param>
+        public void OnReceiveEvent(IBaseEvent newEvent);
+
+    }
+}
